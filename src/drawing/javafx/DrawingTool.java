e8f4df78c50818e03d7b86c4f11817a243efe667
Mike <Mike@MAIKKEYY-PC>
2017-03-15 19:58:53 +0100

    Serializatie functionaliteit toegevoegd.

--- src/drawing/javafx/DrawingTool.java
+++ src/drawing/javafx/DrawingTool.java
@@ -14,6 +14,8 @@ import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
 import drawing.domain.*;
 import drawing.domain.Color;
+import java.io.File;
+import java.io.IOException;
 import javafx.scene.canvas.Canvas;
 import javafx.scene.canvas.GraphicsContext;
 
@@ -26,13 +28,16 @@ public class DrawingTool extends Application {
     private Canvas drawingCanvas;
     private GraphicsContext gc;
     private IPaintable paintable; 
+    private PersistencyMediator persistMediator;
     
-    public DrawingTool() {
-        this.drawing = new Drawing();
+    public DrawingTool() throws IOException {
+        this.persistMediator = new SerializationMediator("C:\\Users\\Mike\\Dropbox\\JCC\\Week 4\\Tekenapplicatie\\tekening.ser"); 
+        /*new DatabaseMediator("jdbc:mysql://localhost:3306/drawings", 
+                "Mike", "nhjk043n7"); */
+        this.drawing = persistMediator.load("tekening");
         this.drawingCanvas = new Canvas(250, 250);
         this.gc = drawingCanvas.getGraphicsContext2D();
-        this.paintable = new JavaFXPaintable(gc);
-        this.drawing.paintUsing(paintable);  
+        this.paintable = new JavaFXPaintable(gc);      
     }
     
     @Override
@@ -46,6 +51,8 @@ public class DrawingTool extends Application {
         primaryStage.setScene(scene);
         draw();
         root.getChildren().add(drawingCanvas);
+      //  persistMediator.save(drawing);
+        
         primaryStage.show();
     }
 
@@ -57,8 +64,27 @@ public class DrawingTool extends Application {
     }
     
     public void draw() {
-        Oval oval = new Oval(Color.RED, new Point(50, 50), 40, 80, 2);
-        paintable.paint(oval);
+       /* Oval oval = new Oval(Color.RED, new Point(50, 50), 40, 80, 2);
+        Oval oval2 = new Oval(Color.GREEN, new Point(70, 70), 100, 80, 10);
+        PaintedText text = new PaintedText(Color.BLACK, new Point(20, 20), 20, 100,
+        "Maikkeyy", "Serif");
+        File file = new File("C:\\Users\\Mike\\Desktop\\JCC Images\\viking.jpg");
+        Image image = new Image(Color.WHITE, new Point(150, 150), 100, 100, file);
+        
+        Point[] points = new Point[5];
+        points[0] = new Point(20,80);
+        points[1] = new Point(50,110);
+        points[2] = new Point(34,140);
+        points[3] = new Point(15,140);
+        points[4] = new Point(11,110);
+                
+        Polygon polygon = new Polygon(Color.BLUE, points , 2);
+        drawing.addItem(oval);
+        drawing.addItem(text);
+        drawing.addItem(polygon);
+        drawing.addItem(oval2);
+        drawing.addItem(image); */
+        drawing.paintUsing(paintable);
     }
     
 }
